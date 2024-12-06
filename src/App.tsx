cccebae3b928f8b249b05295395c90fb1e4f033a
Tokimikichika <ahileses@mail.ru>
2024-12-06 21:11:54 +0300

    f

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ProductsPage from './pages/ProductsPage';
 import ProductDetailsPage from './pages/ProductDetailsPage';
 import CreateProductPage from './pages/CreateProductPage';
 
 const App: React.FC = () => {
   return (
-    <Router>
+    <Router basename="/products">
       <Routes>
-      <Route path="/" element={<Navigate to="/products" replace />} />
-        <Route path="/products" element={<ProductsPage />} />
-        <Route path="/products/:id" element={<ProductDetailsPage />} />
+        <Route path="/" element={<ProductsPage />} />
+        <Route path="/:id" element={<ProductDetailsPage />} />
         <Route path="/create-product" element={<CreateProductPage />} />
-        <Route path="*" element={<Navigate to="/products" replace />} />
+        <Route path="*" element={<ProductsPage />} />
       </Routes>
     </Router>
   ); 
